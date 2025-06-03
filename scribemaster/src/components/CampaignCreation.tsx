c8028049dbad9cca6ca4d7138fd62ff436c8a0da
Harun <theayyyron@gmail.com>
2025-06-03 21:47:12 +0800

    initial form for campaign creation, no api functions yet

---
+++ scribemaster/src/components/CampaignCreation.tsx
@@ -0,0 +1,28 @@
+import { Label } from "@radix-ui/react-label"
+import { Button } from "./ui/button"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "./ui/card"
+import { Input } from "./ui/input"
+
+const CampaignCreation = () => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl text-center">Campaign Creator</CardTitle>
+        <CardDescription className="text-center">
+          Enter the name of your next adventure!
+        </CardDescription>
+      </CardHeader> 
+      <CardContent className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="campaign_name">Name</Label>
+          <Input id="username" type="username" placeholder="The Marvelous Adventures of Foo" required />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Create Campaign</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default CampaignCreation
