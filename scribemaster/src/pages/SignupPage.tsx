f3aa7ec40ca3c6d8797f8423799082ac7420e9c9
Harun <theayyyron@gmail.com>
2025-06-03 21:43:28 +0800

    added header to signup page

--- scribemaster/src/pages/SignupPage.tsx
+++ scribemaster/src/pages/SignupPage.tsx
@@ -3,6 +3,7 @@ import SignupForm2 from "@/components/SignupForm2"
 const SignupPage = () => {
   return (
     <>
+      <h1>Sign up for a new account here!</h1>
         <SignupForm2 />
     </>
   )
