96bd21d2a3366e88671b3823cc251a74107b66ba
Harun <theayyyron@gmail.com>
2025-06-04 23:20:17 +0800

    basic file structure added to CampaignExplorer

--- scribemaster/src/components/CampaignExplorer.tsx
+++ scribemaster/src/components/CampaignExplorer.tsx
@@ -1,7 +1,15 @@
 
 const CampaignExplorer = () => {
   return (
-    <div>CampaignExplorer</div>
+    <div className="flex h-full">
+      {/* Sidebar: file explorer */}
+      <div className="w-1/3 border-r p-4 bg-muted">
+        <h2 className="font-bold mb-4">Files</h2>
+        <ul className="space-y-2">
+          hello
+        </ul>
+      </div>
+    </div>
   )
 }
 
