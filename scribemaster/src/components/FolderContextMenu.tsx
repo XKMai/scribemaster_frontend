8127b3881458df591b1e8c017767098b4bcb9c46
Harun <theayyyron@gmail.com>
2025-06-06 21:58:53 +0800

    initial styling of folder context menu

--- scribemaster/src/components/FolderContextMenu.tsx
+++ scribemaster/src/components/FolderContextMenu.tsx
@@ -10,7 +10,22 @@ interface FolderContextMenuProps {
 
 const FolderContextMenu = () => {
   return (
-    <div>FolderContextMenu</div>
+    <div
+        className="absolute z-50 w-40 bg-white border rounded shadow-md"
+    >
+        <Button
+        variant="ghost"
+        className="w-full justify-start text-left px-4 py-2"
+        >
+        ➕ New Note
+        </Button>
+        <Button
+        variant="ghost"
+        className="w-full justify-start text-left px-4 py-2"
+        >
+        📁 New Folder
+        </Button>
+    </div>
   )
 }
 
