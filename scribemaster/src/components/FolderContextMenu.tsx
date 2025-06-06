e5bc509ecbbaafb99e5266a8bb35de337e7a371e
Harun <theayyyron@gmail.com>
2025-06-06 21:53:45 +0800

    initial imports and interface for FolderContextMenu

--- scribemaster/src/components/FolderContextMenu.tsx
+++ scribemaster/src/components/FolderContextMenu.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import type { apiService, Item } from "../services/apiservice";
 
+interface FolderContextMenuProps {
+  folder: Item;
+  trigger: React.ReactNode;
+  onItemAdded: (updatedFolder: Item) => void;
+}
 
 const FolderContextMenu = () => {
   return (
