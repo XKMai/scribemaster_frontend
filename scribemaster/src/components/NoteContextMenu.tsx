8e8123f7848b501cb1171c5e1b6bdb6d83ddc14e
Harun <theayyyron@gmail.com>
2025-06-06 23:58:01 +0800

    initial imports and interface added to NoteContextMenu

--- scribemaster/src/components/NoteContextMenu.tsx
+++ scribemaster/src/components/NoteContextMenu.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import type { Item } from "../services/apiservice";
+import { apiService } from "../services/apiservice";
+import { Input } from "./ui/input";
+
+interface NoteContextMenuProps {
+  note: Item;
+  trigger: React.ReactNode;
+  onItemUpdated: (updatedNote: Item) => void;
+  onItemDeleted: (deletedNote: Item) => void;
+}
 
 const NoteContextMenu = () => {
   return (
