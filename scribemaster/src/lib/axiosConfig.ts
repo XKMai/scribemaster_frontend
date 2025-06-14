045d64a1c623716bf1911ae6007677d1c26b22cf
XKMai <xkmai014@gmail.com>
2025-06-14 16:38:50 +0800

    Supposedly fixes frontend calls from making to localhost

--- scribemaster/src/lib/axiosConfig.ts
+++ scribemaster/src/lib/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "/api", //|| "http://localhost:5000/api",
   withCredentials: true, // Automatically include cookies
   headers: {
     "Content-Type": "application/json",
