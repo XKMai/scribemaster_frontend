9e9a261403b2c62149ec1fa6f92b130d5fdc9770
Harun <theayyyron@gmail.com>
2025-06-20 02:20:03 +0800

    created PlayerCharacterCreationForm with relevant imports

---
+++ scribemaster/src/components/Character/AssetCreation/PlayerCharacterCreationForm.tsx
@@ -0,0 +1,28 @@
+import { PlayerSchema } from "./playerCharacterSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+
+const PlayerCharacterCreationForm = () => {
+  return <div>PlayerCharacterCreationForm</div>;
+};
+
+export default PlayerCharacterCreationForm;
