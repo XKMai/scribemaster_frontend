e71e96dac63035277f9ea6097c33fb383bf48fe1
Harun <theayyyron@gmail.com>
2025-06-29 23:25:02 +0800

    added button to access spell creation form

--- scribemaster/src/pages/CharacterCreationPage.tsx
+++ scribemaster/src/pages/CharacterCreationPage.tsx
@@ -4,7 +4,8 @@ import { Card, CardTitle, CardContent } from "@/components/ui/card";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/UtilityComponents/AppSidebar";
 import { useState } from "react";
-import { Swords, PawPrint } from "lucide-react";
+import { Swords, PawPrint, WandSparkles } from "lucide-react";
+import SpellCreationForm from "@/components/Character/AssetCreation/SpellCreationForm";
 
 const CharacterCreationPage = () => {
   const [choice, setChoice] = useState("");
@@ -17,12 +18,14 @@ const CharacterCreationPage = () => {
         <CharacterCreationForm />
       ) : choice === "player" ? (
         <PlayerCharacterCreationForm />
+      ) : choice === "spell" ? (
+        <SpellCreationForm />
       ) : (
         <div className="min-h-screen w-full flex flex-col items-center justify-center">
           <h1 className="text-2xl font-semibold mb-6 text-center">
             Choose a character type to create!
           </h1>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-3 gap-4">
             <Card
               className="cursor-pointer hover:shadow-xl transition duration-200 p-6 text-center w-60 h-35"
               onClick={() => setChoice("player")}
@@ -39,6 +42,15 @@ const CharacterCreationPage = () => {
               <CardContent className="flex flex-col items-center gap-4">
                 <PawPrint className="w-12 h-12" />
                 <CardTitle>General Character</CardTitle>
+              </CardContent>
+            </Card>
+            <Card
+              className="cursor-pointer hover:shadow-xl transition duration-200 p-6 text-center w-60 h-35"
+              onClick={() => setChoice("spell")}
+            >
+              <CardContent className="flex flex-col items-center gap-4">
+                <WandSparkles className="w-12 h-12" />
+                <CardTitle className="text-center">Spell</CardTitle>
               </CardContent>
             </Card>
           </div>
