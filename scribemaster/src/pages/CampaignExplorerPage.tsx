7d245814fbaf4a758d226110a8caf5921c6960eb
Harun <theayyyron@gmail.com>
2025-07-04 00:20:40 +0800

    minor formatting changes

--- scribemaster/src/pages/CampaignExplorerPage.tsx
+++ scribemaster/src/pages/CampaignExplorerPage.tsx
@@ -1,21 +1,20 @@
-import { AppSidebar } from "@/components/UtilityComponents/AppSidebar"
-import CampaignExplorer from "@/components/CampaignCreationComponents/CampaignExplorer"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { useParams } from "react-router"
-
+import { AppSidebar } from "@/components/UtilityComponents/AppSidebar";
+import CampaignExplorer from "@/components/CampaignCreationComponents/CampaignExplorer";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useParams } from "react-router";
 
 const CampaignExplorerPage = () => {
-    const {campaignId} = useParams<{campaignId: string}>();
+  const { campaignId } = useParams<{ campaignId: string }>();
 
-if (!campaignId) return <div>No campaign ID provided</div>;
+  if (!campaignId) return <div>No campaign ID provided</div>;
 
   return (
     <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger />
-        <CampaignExplorer campaignId={parseInt(campaignId)}/>
+      <AppSidebar />
+      <SidebarTrigger />
+      <CampaignExplorer campaignId={parseInt(campaignId)} />
     </SidebarProvider>
-  )
-}
+  );
+};
 
-export default CampaignExplorerPage
+export default CampaignExplorerPage;
