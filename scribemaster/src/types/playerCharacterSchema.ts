365b92c30696bcd3e908245a67542712a74a7336
Harun <theayyyron@gmail.com>
2025-07-13 01:36:52 +0800

    updated path

--- scribemaster/src/types/playerCharacterSchema.ts
+++ scribemaster/src/types/playerCharacterSchema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { EntitySchema } from "../../../types/characterSchema";
+import { EntitySchema } from "./characterSchema";
 
 export const PersonalitySchema = z.object({
   traits: z.string().optional(),
