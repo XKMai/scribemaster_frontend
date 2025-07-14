ba6bb15acc30969d6b9e5986c0797fd328427748
Harun <theayyyron@gmail.com>
2025-07-14 23:37:26 +0800

    edited campaign creation styling

--- scribemaster/src/components/CampaignCreationComponents/CampaignCreation.tsx
+++ scribemaster/src/components/CampaignCreationComponents/CampaignCreation.tsx
@@ -1,6 +1,6 @@
-import { Button } from "../ui/button"
-import { Input } from "../ui/input"
-import { z } from "zod"
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
@@ -21,10 +21,8 @@ const loginSchema = z.object({
 
 type LoginSchema = z.infer<typeof loginSchema>;
 
-
-
 const CampaignCreation = () => {
- const form = useForm<LoginSchema>({
+  const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       name: "",
@@ -34,57 +32,59 @@ const CampaignCreation = () => {
   const navigate = useNavigate();
 
   const onSubmit = async (data: LoginSchema) => {
-  try {
+    try {
+      const userdata = await apiService.getCookie();
+      const userId = userdata.user.id;
 
-    const userdata = await apiService.getCookie();
-    const userId = userdata.user.id;
+      const payload = {
+        ...data,
+        createdBy: userId,
+      };
 
-    const payload = {
-      ...data,
-      createdBy: userId,
-    };
+      await api.post("/campaign", payload);
 
-    await api.post("/campaign", payload);
-    
-    // open alert to inform user of successful campaign creation, give button to redirect to campaign reader
-    alert("campaign created successfully");
-    navigate("/campaignreader");
-
-  } catch (error: any) {
-    alert("something went wrong!!!");
-  }
-};
+      // open alert to inform user of successful campaign creation, give button to redirect to campaign reader
+      alert("campaign created successfully");
+      navigate("/campaignreader");
+    } catch (error: any) {
+      alert("something went wrong!!!");
+    }
+  };
 
   return (
-    <Card className="w-full max-w-md mx-auto my-auto flex flex-col h-[400px]">
-
-      <CardTitle  className="mx-auto">
-              <h1>Campaign Name</h1>
+    <Card className="w-full max-w-sm">
+      <CardTitle className="mx-auto">
+        <h1>Create a new campaign here!</h1>
       </CardTitle>
       <CardContent>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10 w-full max-w-sm mx-auto my-auto">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              
-              <FormControl>
-                <Input placeholder="The Marvelous Adventures of Foo" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button variant="outline" type="submit" className="w-full">
-          Create Campaign
-        </Button>
-      </form>
-    </Form>
-    </CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-10 w-full max-w-sm mx-auto my-auto"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="The Marvelous Adventures of Foo"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant="outline" type="submit" className="w-full">
+              Create Campaign
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
     </Card>
   );
-}
+};
 
-export default CampaignCreation
+export default CampaignCreation;
