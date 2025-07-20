1459c3c8c107f78c86950bd1f6914f403284e4ee
Harun <theayyyron@gmail.com>
2025-07-20 18:21:47 +0800

    added buttons to homepage to navigate to character/asset creation and insertion

--- scribemaster/src/pages/HomePage.tsx
+++ scribemaster/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import CampaignCreation from "@/components/CampaignCreationComponents/CampaignCreation";
 import CampaignList from "@/components/HomeComponents/CampaignList";
+import { CharacterButton } from "@/components/HomeComponents/CharacterButton";
 import { AppSidebar } from "@/components/UtilityComponents/AppSidebar";
 import { Card } from "@/components/ui/card";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
@@ -11,18 +12,23 @@ const HomePage = () => {
         <AppSidebar />
         <SidebarTrigger />
         <div className="flex flex-col h-screen w-full bg-muted p-6 overflow-hidden">
-          {/* Header */}
           <h1 className="text-3xl font-bold mb-4">Welcome to Scribe Master!</h1>
-
-          {/* Top: Campaign List (takes up half height) */}
           <Card className="w-full h-1/2 p-4 overflow-hidden flex flex-col mb-4">
             <CampaignList />
           </Card>
-
-          {/* Bottom: Campaign Creation aligned bottom-left */}
-          <div className="flex flex-1 items-end">
-            <div className="w-full max-w-sm">
+          <div className="flex flex-1 w-full gap-2">
+            <div className="w-full max-w-sm flex items-end">
               <CampaignCreation />
+            </div>
+            <div className="flex flex-col justify-end items-end gap-2 w-full">
+              <CharacterButton
+                label="Create Characters and Assets"
+                to="/charactercreation"
+              />
+              <CharacterButton
+                label="View Characters and Assets "
+                to="/characterinsertion"
+              />
             </div>
           </div>
         </div>
