a38c2a04ae14e4a1e247610ed38499fa562260b6
Harun <theayyyron@gmail.com>
2025-07-20 14:29:25 +0800

    fixed spell viewer form

--- scribemaster/src/components/CampaignCreationComponents/CampaignExplorerComponents/SpellViewer.tsx
+++ scribemaster/src/components/CampaignCreationComponents/CampaignExplorerComponents/SpellViewer.tsx
@@ -16,10 +16,44 @@ import { apiService } from "@/services/apiservice";
 import { useEffect } from "react";
 import type { ItemInstance } from "@headless-tree/core";
 import type { Item } from "@/types/TreeTypes";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Props {
   itemInstance: ItemInstance<Item>;
 }
+
+const schools = [
+  "Abjuration",
+  "Conjuration",
+  "Divination",
+  "Enchantment",
+  "Evocation",
+  "Illusion",
+  "Necromancy",
+  "Transmutation",
+] as const;
+
+const classes = [
+  "Artificer",
+  "Barbarian",
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Fighter",
+  "Monk",
+  "Paladin",
+  "Ranger",
+  "Rogue",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+] as const;
 
 export const SpellViewerForm = ({ itemInstance }: Props) => {
   const item = itemInstance.getItemData();
@@ -63,58 +97,59 @@ export const SpellViewerForm = ({ itemInstance }: Props) => {
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="level"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Level</FormLabel>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="grid grid-cols-3 gap-4">
+          {(["level", "castingTime", "range"] as const).map((key) => (
+            <FormField
+              key={key}
+              control={form.control}
+              name={key}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">{key}</FormLabel>
+                  <FormControl>
+                    <Input
+                      type={key === "level" ? "number" : "text"}
+                      {...field}
+                      onChange={(e) =>
+                        field.onChange(
+                          key === "level"
+                            ? parseInt(e.target.value) || 0
+                            : e.target.value
+                        )
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+        </div>
         <FormField
           control={form.control}
           name="school"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>School</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
+              <FormLabel>School of Magic</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select school" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {schools.map((school) => (
+                    <SelectItem key={school} value={school}>
+                      {school}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
               <FormMessage />
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="castingTime"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Casting Time</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="range"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Range</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+
         <FormField
           control={form.control}
           name="components"
@@ -177,23 +212,46 @@ export const SpellViewerForm = ({ itemInstance }: Props) => {
         <FormField
           control={form.control}
           name="classes"
-          render={({ field }) => (
+          render={() => (
             <FormItem>
-              <FormLabel>Classes (comma-separated)</FormLabel>
-              <FormControl>
-                <Input
-                  value={field.value.join(", ")}
-                  onChange={(e) =>
-                    field.onChange(
-                      e.target.value.split(",").map((v) => v.trim())
-                    )
-                  }
-                />
-              </FormControl>
+              <FormLabel>Classes</FormLabel>
+              <div className="grid grid-cols-2 gap-2">
+                {classes.map((cls) => (
+                  <FormField
+                    key={cls}
+                    control={form.control}
+                    name="classes"
+                    render={({ field }) => {
+                      const isChecked = field.value?.includes(cls);
+                      return (
+                        <FormItem
+                          key={cls}
+                          className="flex items-center space-x-2"
+                        >
+                          <FormControl>
+                            <input
+                              type="checkbox"
+                              checked={isChecked}
+                              onChange={(e) => {
+                                const newValue = e.target.checked
+                                  ? [...field.value, cls]
+                                  : field.value.filter((v) => v !== cls);
+                                field.onChange(newValue);
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="capitalize">{cls}</FormLabel>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                ))}
+              </div>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <Button type="submit" className="w-full">
           Save Changes
         </Button>
