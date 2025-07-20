740e95c0f691ca6b7359fc899b57febe39d8def5
Harun <theayyyron@gmail.com>
2025-07-20 09:07:37 +0800

    fixed logout api call

--- scribemaster/src/components/LoginComponents/LogoutButton.tsx
+++ scribemaster/src/components/LoginComponents/LogoutButton.tsx
@@ -1,27 +1,24 @@
-import { useNavigate } from "react-router"
-import { Button } from "../ui/button"
-import api from "@/lib/axiosConfig"
+import { useNavigate } from "react-router";
+import { Button } from "../ui/button";
+import api from "@/lib/axiosConfig";
 
 const LogoutButton = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      await api.post('/logout',null,{
-        withCredentials: true,
-      })
-
-      navigate("/login")
+      await api.get("/logout");
+      navigate("/login");
     } catch (error) {
-      alert("Logout failed: " + error)
+      alert("Logout failed: " + error);
     }
-  }
+  };
 
   return (
-    <Button variant="outline" onClick={handleLogout}>
+    <Button variant="ghost" onClick={handleLogout}>
       Logout
     </Button>
-  )
-}
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
