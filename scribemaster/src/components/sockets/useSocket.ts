496163c2a9084ee6a24b0893d58b1f28801ff4cf
Harun <theayyyron@gmail.com>
2025-07-25 17:36:40 +0800

    updated event parameters, removed bug that caused socket to continuously reconnect to server

--- scribemaster/src/components/sockets/useSocket.ts
+++ scribemaster/src/components/sockets/useSocket.ts
@@ -2,11 +2,10 @@ import { useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 import type { EntitySummary } from "@/types/characterSchema";
 import type { PlayerCharacter } from "@/types/playerCharacterSchema";
-import { useCombatStore } from "../CombatEncounterComponents/combatStore";
 
 type ServerToClientEvents = {
   roomData: (payload: { entityIds: number[]; entities: EntitySummary[] }) => void;
-  entityUpdated: (payload: { entityId: number; updatedEntity: EntitySummary }) => void;
+  entityUpdated: (payload: { updatedEntity: EntitySummary }) => void;
   chatMessage: (payload: {
     sender: string;
     message: string;
@@ -17,7 +16,7 @@ type ServerToClientEvents = {
 type ClientToServerEvents = {
   joinRoom: (roomId: string) => void;
   addEntity: (payload: { roomName: string; entityId: number }) => void;
-  removeEntity: (payload: { roomName: string; itemId: number }) => void;
+  removeEntity: (payload: { roomName: string; entityId: number }) => void;
   updateEntity: (payload: {
     roomName: string;
     entityId: number;
@@ -63,8 +62,9 @@ export const useSocket = (
       console.log("📡 Emitting joinRoom:", roomId);
     };
 
-    const handleEntityUpdated = ({ entityId, updatedEntity }: { entityId: number; updatedEntity: EntitySummary }) => {
+    const handleEntityUpdated = ({ updatedEntity }: { updatedEntity: EntitySummary }) => {
       console.log("📥 Received entityUpdated:", updatedEntity.name);
+      console.log("Entity id updated:" )
       onEntityUpdated(updatedEntity);
     };
 
@@ -78,27 +78,15 @@ export const useSocket = (
       onRoomData(data);
     };
 
-    const handleChatMessage = ({ sender, message, timestamp }: { sender: string; message: string; timestamp: number }) => {
-      console.log("📥 Received chatMessage from server:", { sender, message });
-      useCombatStore.getState().addLog({ sender, message, timestamp });
-    };
+    socket.off("connect", handleConnect);
+    socket.off("entityUpdated", handleEntityUpdated);
+    socket.off("roomData", handleRoomData);
 
-    socket.off("connect");
-    socket.off("entityUpdated");
-    socket.off("roomData");
-    //socket.off("chatMessage");
 
     socket.on("connect", handleConnect);
     socket.on("entityUpdated", handleEntityUpdated);
     socket.on("roomData", handleRoomData);
-    //socket.on("chatMessage", handleChatMessage);
 
-
-
-    // If already connected, manually join
-    if (socket.connected) {
-      handleConnect();
-    }
 
     return () => {
         socket.off("connect", handleConnect);
