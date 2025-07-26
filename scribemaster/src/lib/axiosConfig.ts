dacdc49f5e54f28e258736302be8bd4a00e92d89
Harun <theayyyron@gmail.com>
2025-07-26 21:24:58 +0800

    changed api url for deployment

--- scribemaster/src/lib/axiosConfig.ts
+++ scribemaster/src/lib/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://scribemaster-frontend-alb-469534981.ap-southeast-1.elb.amazonaws.com",
   withCredentials: true, // Automatically include cookies
   headers: {
     "Content-Type": "application/json",
