97faed9d22d821ceccc8fcaa74dfd2b233c16c00
Harun <theayyyron@gmail.com>
2025-08-01 23:17:20 +0800

    changed baseURL for local testing

--- scribemaster/src/lib/axiosConfig.ts
+++ scribemaster/src/lib/axiosConfig.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://scribemaster-frontend-alb-469534981.ap-southeast-1.elb.amazonaws.com/api",
+    "http://localhost:5000/api",
   withCredentials: true, // Automatically include cookies
   headers: {
     "Content-Type": "application/json",
