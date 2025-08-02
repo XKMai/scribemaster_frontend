945a7773be7cd7322a8fbd7e60618b360bd2884d
Harun <theayyyron@gmail.com>
2025-08-03 07:56:40 +0800

    test user page to get state from store

--- scribemaster/src/pages/UserSettingsPage.tsx
+++ scribemaster/src/pages/UserSettingsPage.tsx
@@ -1,13 +1,17 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/UtilityComponents/AppSidebar";
+import { userStore } from "@/stores/userStore";
 
 const UserSettingsPage = () => {
+  const initialiseUser = userStore((state) => state.initialiseUser);
+  const user = userStore((state) => state.user);
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarTrigger />
       <div>
-        <h1>Username</h1>
+        <button onClick={initialiseUser}>set user data</button>
+        <h1>{user?.name ?? "wait a minute..."}</h1>
         <p>email</p>
         <p>password rest?</p>
         <p>campaigns joined (list)</p>
