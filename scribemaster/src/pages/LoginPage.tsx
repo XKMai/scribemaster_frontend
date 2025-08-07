4621dccd3e84cd1a0b505f7a38b9d26dae05e603
Harun <theayyyron@gmail.com>
2025-08-07 16:47:37 +0800

    added forgot password form to login page

--- scribemaster/src/pages/LoginPage.tsx
+++ scribemaster/src/pages/LoginPage.tsx
@@ -1,6 +1,22 @@
+import ForgotPasswordForm from "@/components/LoginComponents/ForgotPasswordForm";
 import LoginForm2 from "@/components/LoginComponents/LoginForm2";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Link } from "react-router";
 
 const LoginPage = () => {
@@ -8,13 +24,31 @@ const LoginPage = () => {
     <>
       <h1 className="font-bold text-2xl "> Welcome to Scribe Master! </h1>
       <br />
-      <Card className="w-sm">
+      <Card className="w-sm ">
         <CardHeader>
           <CardTitle className="text-center">Login Here!</CardTitle>
         </CardHeader>
         <CardContent className="w-full">
           <LoginForm2 />
         </CardContent>
+        <CardFooter className="justify-items-center">
+          <Dialog>
+            <DialogTrigger>
+              <Badge variant="secondary" className="w-full">
+                Forgot your password?
+              </Badge>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Password Reset</DialogTitle>
+                <DialogDescription>
+                  Enter the email address used for your account!
+                </DialogDescription>
+                <ForgotPasswordForm />
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </CardFooter>
       </Card>
 
       <br />
