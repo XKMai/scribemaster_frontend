8f3ddfae1a258ec09248c1511a925d738915fb6a
Harun <theayyyron@gmail.com>
2025-08-07 16:42:55 +0800

    created forgot password form that receives email address

---
+++ scribemaster/src/components/LoginComponents/ForgotPasswordForm.tsx
@@ -0,0 +1,70 @@
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const forgotPasswordSchema = z.object({
+  email: z.string().email({ message: "Valid email is required" }),
+});
+
+type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
+
+const ForgotPasswordForm = () => {
+  const form = useForm<ForgotPasswordSchema>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: ForgotPasswordSchema) => {
+    try {
+      console.log("Email sent: ", data);
+
+      navigate("/login");
+    } catch (error: any) {
+      alert("signup failed");
+    }
+  };
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 w-full max-w-sm"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="you@example.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <Button type="submit">Submit Email</Button>
+    </>
+  );
+};
+
+export default ForgotPasswordForm;
